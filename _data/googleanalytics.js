aada13f6e19854215a413cf918e17c777c03fbfb
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-02-04 02:44:19 +0900

    update GoogleAnalytics

--- _data/googleanalytics.js
+++ _data/googleanalytics.js
@@ -23,7 +23,7 @@ const metadata = require("./metadata.json");
 
 module.exports = function () {
   let id = metadata.googleAnalyticsId;
-  if (/Update me/.test(id)) {
+  if (/G-W6W9YD5G74/.test(id)) {
     id = null;
   }
   return id;
